63a23e187b2bc72a9750e5621e8ebdc9fc877a1b
Michelle-1982 <michellepachecopantoja@gmail.com>
2025-10-18 11:39:50 +0200

    identacion y subir codigo a gitpages

--- src/components/FilterByName.jsx
+++ src/components/FilterByName.jsx
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import ls from "../services/localStorage";
 import '../styles/filterbyname.css';
 const FilterByName = ({ name, updateName }) => {
-    
     const handleChangeName = (ev) => {
         updateName(ev.target.value)
         ls.set("filterByName", ev.target.value);
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -6,7 +6,6 @@ import CharacterDetail from "../components/CharacterDetail";
 
 const DetailPage = () => {
     const { id } = useParams();
-    
     const characters = ls.get("characters", []);
     const character = characters.find(character => character.id === id);
    
